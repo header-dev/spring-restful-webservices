a5e82d3d03911ac48c0763b4013fcf50494c7371
Headerdev <headerdev@gmail.com>
2023-06-29 17:46:43 +0700

    The API document completed and support to request header XML format and then the Multiple Language i18n completed.

--- src/main/java/com/idevalot/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/idevalot/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.idevalot.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	private MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping(path = "/helloworld")
 	public String helloWorkd() {
@@ -17,14 +27,21 @@ public class HelloWorldController {
 		return new HelloWorldBean("Hello World");
 	}
 
-	
-	//Path Parameter
+	// Path Parameter
 	// users/{id}/todos/{id}
 	// hello-world/path-variable/{name}
 	// hello-world/path-variable/Witz
 	@GetMapping(path = "/helloworld/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello world, %s", name));
+	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorkdInternationalized() {
+		
+		Locale locale = LocaleContextHolder.getLocale();
+		
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 
 }
