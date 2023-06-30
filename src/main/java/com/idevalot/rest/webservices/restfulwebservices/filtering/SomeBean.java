7a23e89bcac72b64db406390c20433d16e5e60cb
Headerdev <headerdev@gmail.com>
2023-06-30 16:34:05 +0700

    The dynamic filter completed.

--- src/main/java/com/idevalot/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/idevalot/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,12 +1,17 @@
 package com.idevalot.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties({"field1", "field2"})
+//@JsonIgnoreProperties({"field1", "field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 
 	private String field1;
 	private String field2;
+	
+//	@JsonIgnore
 	private String field3;
 
 	public SomeBean(String field1, String field2, String field3) {
