ef561eb4e385d975df4d83a6fa2904e316eb3f68
Headerdev <headerdev@gmail.com>
2023-06-30 15:49:54 +0700

    The static filter completed

--- src/main/java/com/idevalot/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/idevalot/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,8 @@ package com.idevalot.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,9 +12,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should have atleast 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birth date should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 
 	public User(Integer id, String name, LocalDate birthDate) {
