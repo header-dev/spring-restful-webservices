ed2fc20bea894860f8322fb880114d48dae0f701
Headerdev <headerdev@gmail.com>
2023-06-30 12:32:00 +0700

    The HATEOAS completed

--- src/main/java/com/idevalot/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/idevalot/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,10 @@ package com.idevalot.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,17 +33,28 @@ public class UserResource {
 		return service.findAll();
 	}
 
-//	GET /users
+	// http://localhost:8080/users
+	// EntityModel
+	// WebMvcLinkBuilder
+
+	// GET /users
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 
 		if (user == null) {
 			throw new UserNotFoundException("id:" + id);
 		}
-		return user;
+
+		EntityModel<User> entityModel = EntityModel.of(user);
+
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		
+		return entityModel;
 	}
-	
+
+
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
 		service.deleteById(id);
--- src/main/java/com/idevalot/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/idevalot/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -6,15 +6,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+	// Twitter Styles
 	@GetMapping("/v1/person")
 	public PersonV1 getFirstVersionOfPerson() {
 		return new PersonV1("Hello From Person V1", 33);
 	}
-	
 	
 	@GetMapping("/v2/person")
 	public PersonV2 getSecondVerionOfPerson() {
 		return new PersonV2("Hello From Person V2");
 	}
 	
+	//	Amazon Styles
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter() {
+		return new PersonV1("Hello From Person V1", 44);
+	}
+	
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVerionOfPersonRequestParameter() {
+		return new PersonV2("Hello From Person V2");
+	}
 }
