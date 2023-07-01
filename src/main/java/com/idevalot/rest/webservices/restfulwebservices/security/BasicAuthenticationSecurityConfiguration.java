da87dbd300f7932ebf6cc5f3f1cb4f8b3c521856
Headerdev <headerdev@gmail.com>
2023-07-01 17:21:41 +0700

    The Basic Auth completed

--- src/main/java/com/idevalot/rest/webservices/restfulwebservices/security/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/idevalot/rest/webservices/restfulwebservices/security/BasicAuthenticationSecurityConfiguration.java
@@ -3,24 +3,31 @@ package com.idevalot.rest.webservices.restfulwebservices.security;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.mysql.cj.Session;
+
 @Configuration
-public class SpringSecurityConfiguration {
+public class BasicAuthenticationSecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		// All requests should be authenticated
-		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
-		
+		http.authorizeHttpRequests(
+				auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated());
+
 		// If a request is not authenticated, a web page is shown
 		http.httpBasic(withDefaults());
-		
+
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
 		// CSRF -> POST, PUT
-		http.csrf().disable();
-		
+		http.csrf((csrf) -> csrf.disable());
+
 		return http.build();
 	}
 
